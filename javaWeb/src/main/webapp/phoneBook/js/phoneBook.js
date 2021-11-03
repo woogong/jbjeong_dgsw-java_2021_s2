c85119a690210b3670979f923a2b13754e718815
Woogong <batang21@gmail.com>
2021-11-03 14:41:16 +0900

    ..

--- javaWeb/src/main/webapp/phoneBook/js/phoneBook.js
+++ javaWeb/src/main/webapp/phoneBook/js/phoneBook.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function() {
 	initEventHandlers();
+	
+	readList(function(list) {
+		showList(list);
+	});
 }); 
 
 
@@ -43,6 +47,56 @@ function doRegister() {
 		}
 	});
 }
+
+function readList(callback) {
+	/*$.get("list.do", null, function(response) {
+		if (response.resultCode == "Success") {
+			callback(response.data);
+		} else {
+			alert("서버에서 데이터를 읽어 올 수 없습니다. " + response.resultMessage);
+		}
+	});*/
+	var list = [
+		{idx: 1, name: "홍길동", phoneNumber: "010-0000-0012"},
+		{idx: 2, name: "임꺽정", phoneNumber: "010-1111-0012"},
+		{idx: 3, name: "장길산", phoneNumber: "010-1234-5678"},
+	];
+	
+	callback(list);
+}
+
+function showList(list) {
+	$(".list tbody").empty();
+	
+	for (var i in list) {
+		showLine(list[i]);
+	}
+}
+
+function showLine(data) {
+	var tr = $("<tr/>");
+	
+	// checkbox
+	var td1 = $("<td></td>");
+	var checkbox = $("<input name='checkSelect' type='checkbox'>");
+	checkbox.val(data.idx);
+	td1.append(checkbox);
+	tr.append(td1);
+	
+	// name
+	var td2 = $("<td></td>");
+	td2.html(data.name);
+	tr.append(td2);
+	// TODO 이름 클릭 -> update
+	
+	// phone number
+	var td3 = $("<td></td>");
+	td3.html(data.phoneNumber);
+	tr.append(td3);
+	
+	$(".list tbody").append(tr);
+}
+
 
 
 
